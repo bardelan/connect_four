358de22f2867f52b82659a2486786806b1f58392
Chris Marmo <themarmo@gmail.com>
2016-07-21 15:49:21 -0400

    Removed the main loop from ConnectFour and put it in cf_main.

--- source/cf_main.rb
+++ source/cf_main.rb
@@ -1,4 +1,29 @@
 require_relative "cf_classes.rb"
+	
+loop do
+	puts "New game or load save file?" 
+	
+	ConnectFour.prompt("Enter 'new' or 'load': ", ["new", "load"]) do |response|
+		if response == "new"
+			game = ConnectFour.new
+		else
+			begin
+				puts "Please enter the file name: "
+				file_name = gets.chomp
+				
+				raise InvalidFileName unless File.file?(file_name)
+			rescue InvalidFileName => e
+				puts "Sorry, that file name is not valid."
+				retry
+			end		
+			
+			game = ConnectFour.load(file_name)
+		end
 
-game = ConnectFour.new
-game.play
+	game.play
+	end
+
+	ConnectFour.prompt("\nPlay again?", ["y", "n", "yes", "no"]) do |response|
+		break if response[0] == "n"
+	end
+end
